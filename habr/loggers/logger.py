e2140e996e5f9dc259790014c3394a26c5dc93c1
AssemblerBossss <nazarovmisa39@mail.ru>
2025-03-27 11:43:26 +0300

    готовый логгер

--- habr/loggers/logger.py
+++ habr/loggers/logger.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 logging.basicConfig(
     level=logging.INFO,
@@ -10,3 +11,45 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger("habr_logger")
+
+
+def setup_logger(logger_name: str, log_file: str = 'habr_parser.log') -> logging.Logger:
+    """
+    Настройка логгера с записью в файл
+
+    :param logger_name: Имя логгера
+    :param log_file: Имя файла для логов
+    :return: Настроенный логгер
+    """
+
+    # Создаем папку для логов, если ее нет
+    os.makedirs('logs', exist_ok=True)
+    log_path = os.path.join('logs', log_file)
+
+    # Настройка формата
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+
+    # Настройка обработчика для файла
+    file_handler = logging.FileHandler(
+        log_path,
+        encoding='utf-8'
+    )
+    file_handler.setFormatter(formatter)
+
+    # Настройка обработчика для файла
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    # Создаем и настраиваем логгер
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
+
+
+
+
